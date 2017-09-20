4cda8e40ce7d95e755fdcc9fe154063ae8023444
tsikalenko <32128038+tsikalenko@users.noreply.github.com>
2017-09-20 16:59:07 +0300

    Update Lection2HomeWorkTask2.java

--- Lection2HomeWorkTask2.java
+++ Lection2HomeWorkTask2.java
@@ -5,16 +5,16 @@ public class Lection2HomeWorkTask2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("¬ведите длину первой стороны треугольника:");
+		System.out.println("Enter size first side of the triangle:");
 		double a = sc.nextDouble();
-		System.out.println("¬ведите длину второй стороны треугольника:");
+		System.out.println("Enter size second side of the triangle:");
 		double b = sc.nextDouble();
-		System.out.println("¬ведите длину третей стороны треугольника:");
+		System.out.println("Enter size third side of the triangle:");
 		double c = sc.nextDouble();
 		
 		double p = (a+b+c)/2;
 		
-		System.out.println("ѕлощадь заданого треугольника = " + (Math.sqrt(p*(p-a)*(p-b)*(p-c))));
+		System.out.println("Square of the triangle = " + (Math.sqrt(p*(p-a)*(p-b)*(p-c))));
 		
 		sc.close();
 	}
